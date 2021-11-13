1c65fed8b789afa5edcd79b191fe6b86e1ef9091
DanM10 <55813571+DanM10@users.noreply.github.com>
2021-11-13 09:17:32 -0500

    prueba1

---
+++ SGLabFIS/src/sGLabFis/main.java
@@ -0,0 +1,23 @@
+package sGLabFis;
+/**
+ * @author Daniel
+ *
+ */
+public class main {
+
+	/**
+	 * 
+	 */
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
